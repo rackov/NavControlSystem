6637f2ec15ba8edaffd0f584debb3d1dc7c81a98
Vladimir R <yrackov@gmail.com>
2025-08-26 15:22:39 +0300

    Models

--- pkg/model/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/rackov/NavControlSystem/pkg/model
-
-go 1.24.6
--- services/receiver/internal/protocol.go
+++ services/receiver/internal/protocol.go
@@ -1,14 +1,10 @@
 package internal
 
-import (
-	"github.com/rackov/NavControlSystem/pkg/models"
-)
-
 type Protocol interface {
 	Start(port string) error
 	Stop() error
 	SendCommand(deviceID string, command []byte) error
-	SetDataHandler(handler func(*models.NavRecord))
+	SetDataHandler(handler func(*model.NavRecord))
 }
 
 type ProtocolFactory interface {
